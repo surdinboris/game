5597ba18cc03ecf416e2a2b15abf84b3c7021f11
borrik <borrik@live.ru>
2018-06-30 17:42:13 +0300

    all validation transferred to validation helper

--- mygame.py
+++ mygame.py
@@ -1,6 +1,4 @@
 debugActive = True
-#possible choise variants
-variants={1:'Rock', 2:'Paper', 3:'Scissors'}
 #won combinations
 wons=((1,3),(2,1),(3,2))
 results={}
@@ -14,9 +12,11 @@ def debug(prn):
 def appVal(question, valType='str'):
     while True:
         val = input(question)
-        if valType == 'int':
+        if valType == 'int' and valType == 'int':
             try:
-                int(val)
+                if int(val) not in range(1, 4):
+                    print('Invalid choice')
+                    continue
             except ValueError:
                 print('Invalid choice')
                 continue
@@ -32,22 +32,13 @@ def game():
         return game()
 
     def startlevel(levelnum, players):
-        #helper - returns a player ho won
-        def checkValidity(choises):
-            #check validity of inputs
-            for ch in list(choises):
-                if int(choises[ch]) not in variants:
-                    print("{}, you wrong. Next time please be careful and choose a proper option".format(ch))
-                    choises[ch]='disqualified'
-            return{levelnum:choises}
         #starting level execution
-
         print("-=Level %s=-" %levelnum)
         question="{}, please enter your choice: \n1-Rock,  2-Paper, 3-Scissors:\t"
         #retrieving users choises
         choises = {pl:int(appVal(question.format(pl), 'int')) for pl in players}
         #appending results
-        results.update(checkValidity(choises))
+        results.update({levelnum:choises})
 
     for n in range(1,3):
         startlevel(n,players)
@@ -59,9 +50,9 @@ def game():
             debug("starting to verify values {} vs {} ".format(choises[players[0]],choises[players[1]]))
             if choises[players[0]] == choises[players[1]]:
                 return("no one wons")
-            elif tuple([choises[players[0]],choises[players[1]]]) in wons:
+            if tuple([choises[players[0]],choises[players[1]]]) in wons:
                 return(players[0]+ " wons")
-            elif tuple([choises[players[1]],choises[players[0]]]) in wons:
+            if tuple([choises[players[1]],choises[players[0]]]) in wons:
                 return(players[1]+ " wons")
             return
         for level in results:
